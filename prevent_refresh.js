409d3bdf005f33719cca616b1982159f74bb5093
daniel-julius-sam <82655381+daniel-julius-sam@users.noreply.github.com>
2021-04-17 01:31:08 +0530

    removed unnecessary script

--- prevent_refresh.js
+++
@@ -1,9 +0,0 @@
-$(window).on('unload', function() {
-   $(window).scrollTop(0);
-});
-
-window.onunload = function(){ window.scrollTo(0,0); }
-
-if ('scrollRestoration' in history) {
-  history.scrollRestoration = 'manual';
-}
